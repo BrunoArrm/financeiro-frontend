d0107a7b04d201f7d9714f646d93fb744445d3b1
Bruno Arruda <86083291+BrunoArrm@users.noreply.github.com>
2023-05-10 22:46:29 -0300

    configurados os estados para edição em alguns campos

--- src/components/Titulo/Titulo.tsx
+++ src/components/Titulo/Titulo.tsx
@@ -1,17 +1,12 @@
 import { useParams } from 'react-router-dom';
 import { dados } from '../../assets/helpers/Dados';
 import './Titulo.scss';
+import { useState } from 'react';
 
 const Titulo = () => {
   const { id } = useParams();
   const titulo = dados.find(item => item.id === Number(id));
 
-  const nDoc = titulo?.nDoc;
-  const contato = titulo?.contato;
-  const responsavel = titulo?.responsavel;
-  const valor = titulo?.valor;
-  const vencimento = titulo?.vencimento;
-  const descricao = titulo?.descricao;
   const grupo = titulo?.grupo;
   const subgrupo = titulo?.subgrupo;
   const parcela = titulo?.parcela;
@@ -20,6 +15,44 @@ const Titulo = () => {
   const dBaixa = titulo?.dBaixa;
   const pago = titulo?.pago;
 
+  const [nDocValue, setNDocValue] = useState(titulo?.nDoc);
+  const [contatoValue, setContatoValue] = useState(titulo?.contato);
+  const [responsavelValue, setResponsavelValue] = useState(titulo?.responsavel);
+  const valor = titulo?.valor.toLocaleString("pt-BR", { minimumFractionDigits: 2 })
+  const [valorValue, setValorValue] = useState(valor);
+  let vencimento;
+  if (titulo?.vencimento) {
+    vencimento = new Date(titulo?.vencimento).toLocaleDateString("pt-BR");
+  } else {
+    vencimento = null
+  }
+  const [vencimentoValue, setVencimentoValue] = useState(vencimento);
+  const [descricaoValue, setDescricaoValue] = useState(titulo?.descricao);
+
+  const handleNDocChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNDocValue(e?.target.value)
+  }
+
+  const handleContatoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setContatoValue(e?.target.value);
+  }
+
+  const handleResponsavelChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setResponsavelValue(e?.target.value);
+  }
+
+  const handleValorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValorValue(e?.target.value);
+  }
+
+  const handleVencimentoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setVencimentoValue(e?.target.value);
+  }
+
+  const handleDescricaoValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDescricaoValue(e?.target.value);
+  }
+
   // Lógica para tratar caso o título não seja encontrado
   if (!titulo) {
     return <div>Título não encontrado</div>;
@@ -27,53 +60,108 @@ const Titulo = () => {
 
   return (
     <fieldset className='titulo'>
-      <legend>Título Nº {id}</legend>
-      <div>
-        <label htmlFor='agrupamento'>Agrupamento</label>
-        <input type='text' value={titulo.agrupamento} id='agrupamento'></input>
+      <legend> Título nº {id} </legend>
+      <div className='grid-container'>
+
+        <div className='campo-pequeno'>
+          <label htmlFor='nDoc'>N. Doc.</label>
+          <input 
+            type='text' 
+            value={nDocValue} 
+            id='nDoc' 
+            onChange={handleNDocChange}
+          />
+        </div>
+
+        <div className='campo-pequeno'>
+          <label htmlFor='contato'>Contato</label>
+          <input 
+            type='text' 
+            value={contatoValue} 
+            id='contato' 
+            onChange={handleContatoChange}
+          />
+        </div>
+
+        <div className='campo-pequeno'>
+          <label htmlFor='responsavel'>Responsável</label>
+          <input 
+            type='text' 
+            value={responsavelValue} 
+            id='responsavel' 
+            onChange={handleResponsavelChange}
+          />
+        </div>
+
+        <div className='campo-pequeno'>
+          <label htmlFor='valor'>Valor</label>
+          <input
+            type='text'
+            value={valorValue}
+            id='valor'
+            onChange={handleValorChange}
+          />
+        </div>
+
+        <div className='campo-pequeno'>
+          <label htmlFor='vencimento'>Data de vencimento</label>
+          <input
+            type='text'
+            value={vencimentoValue}
+            id='vencimento'
+            onChange={handleVencimentoChange}
+          />
+        </div>
+
+        <div className='campo-grande'>
+          <label htmlFor='descricao'>Descrição</label>
+          <textarea 
+            value={descricaoValue} 
+            id='descricao'
+            onChange={handleDescricaoValue}
+          />
+        </div>
+
+        <div className='campo-pequeno'>
+          <label htmlFor='grupo'>Grupo</label>
+          <input type='text' value={grupo} id='grupo' />
+        </div>
+
+        <div className='campo-pequeno'>
+          <label htmlFor='subgrupo'>Subgrupo</label>
+          <input type='text' value={subgrupo} id='subgrupo' />
+        </div>
+
+        <div className='campo-pequeno'>
+          <label htmlFor='tipo'>Tipo</label>
+          <input type='text' value={tipo} id='tipo' />
+        </div>
+
+        <div className='campo-pequeno'>
+          <label htmlFor='dBaixa'>Data de baixa</label>
+          <input
+            type='text'
+            value={dBaixa ? new Date(dBaixa).toLocaleDateString("pt-BR") : ""}
+            id='dBaixa'
+          />
+        </div>
+
+        <div className='campo-pequeno'>
+          <label htmlFor='pago'>Valor pago</label>
+          <input
+            type='text'
+            value={pago ? pago.toLocaleString("pt-BR", { minimumFractionDigits: 2 }) : ""}
+            id='pago'
+          />
+        </div>
+
       </div>
-      <div>
-        <label htmlFor='nDoc'>N.Doc </label>
-        <input type='text' value={nDoc} id='nDoc'></input>
+      <div className='rodape'>
+        <p>Parcelas: {parcela} / {tParcelas}</p>
+        <div className='campo-agrupamento'>ID Agrupadora {titulo.agrupamento}</div>
       </div>
-      <div>
-        <label htmlFor=''>Contato </label>
-        <input type='text' value={contato} id='contato'></input>
-      </div>
-      <div>
-        <label htmlFor='responsavel'>Responsável </label>
-        <input type='text' value={responsavel} id='responsavel'></input>
-      </div>
-      <div>
-        <label htmlFor='valor'>Valor </label>
-        <input type='text' value={valor ? valor.toLocaleString("pt-BR", { minimumFractionDigits: 2 }) : ""} id='valor'></input>
-      </div>
-      <div>
-        <label htmlFor='vencimento'>Data de vencimento </label>
-        <input type='text' value={vencimento ? new Date(vencimento).toLocaleDateString("pt-BR") : ""} id='vencimento'></input>
-      </div>
-      <div>
-        <label htmlFor='vencimento'>Descrição </label>
-        <textarea value={descricao} id='descricao'></textarea>
-      </div>
-      <div>
-        <label htmlFor='grupo'>Grupo </label>
-        <input type='text' value={grupo} id='grupo'></input>
-      </div>
-      <div>
-        <label htmlFor='grupo'>Grupo </label>
-        <input type='text' value={grupo} id='grupo'></input>
-      </div>
-      <div>
-        <label htmlFor='subgrupo'>Subgrupo </label>
-        <input type='text' value={subgrupo} id='subgrupo'></input>
-      </div>
-      
-      <p>Parcelas: {parcela} / {tParcelas}</p>
-      <p>Tipo: {tipo}</p>
-      <p>Número do Documento: {dBaixa ? new Date(dBaixa).toLocaleDateString("pt-BR") : ""}</p>
-      <p>Valor pago: {pago ? pago.toLocaleString("pt-BR", { minimumFractionDigits: 2 }) : ""}</p>
     </fieldset>
+
   );
 }
 

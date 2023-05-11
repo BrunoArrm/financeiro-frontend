b8a730a91da58902f48fedeb7e38d7fdc1df94ed
Bruno Arruda <86083291+BrunoArrm@users.noreply.github.com>
2023-05-11 12:38:42 -0300

    Títulos editáveis implementado

--- src/components/Titulo/Titulo.tsx
+++ src/components/Titulo/Titulo.tsx
@@ -7,13 +7,8 @@ const Titulo = () => {
   const { id } = useParams();
   const titulo = dados.find(item => item.id === Number(id));
 
-  const grupo = titulo?.grupo;
-  const subgrupo = titulo?.subgrupo;
   const parcela = titulo?.parcela;
   const tParcelas = titulo?.tParcelas;
-  const tipo = titulo?.tipo;
-  const dBaixa = titulo?.dBaixa;
-  const pago = titulo?.pago;
 
   const [nDocValue, setNDocValue] = useState(titulo?.nDoc);
   const [contatoValue, setContatoValue] = useState(titulo?.contato);
@@ -28,6 +23,18 @@ const Titulo = () => {
   }
   const [vencimentoValue, setVencimentoValue] = useState(vencimento);
   const [descricaoValue, setDescricaoValue] = useState(titulo?.descricao);
+  const [grupoValue, setGrupoValue] = useState(titulo?.grupo);
+  const [subgrupoValue, setSubgrupoValue] = useState(titulo?.subgrupo);
+  const tipoValue = titulo?.tipo === "D" ? "Débito" : "Crédito";
+  let dataBaixa;
+  if (titulo?.dBaixa) {
+    dataBaixa = new Date(titulo?.dBaixa).toLocaleDateString("pt-BR");
+  } else {
+    dataBaixa = null
+  }
+  const [dBaixa, setDBaixa] = useState(dataBaixa);
+  const valorPago = titulo?.valor.toLocaleString("pt-BR", { minimumFractionDigits: 2 })
+  const [vPago, setVPago] = useState(valorPago);
 
   const handleNDocChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNDocValue(e?.target.value)
@@ -49,8 +56,24 @@ const Titulo = () => {
     setVencimentoValue(e?.target.value);
   }
 
-  const handleDescricaoValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleDescricaoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setDescricaoValue(e?.target.value);
+  }
+
+  const handleGrupoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setGrupoValue(e?.target.value);
+  }
+
+  const handleSubgrupoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSubgrupoValue(e?.target.value);
+  }
+
+  const handleDBaixaChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDBaixa(e?.target.value);
+  }
+
+  const handleVPagoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setVPago(e?.target.value);
   }
 
   // Lógica para tratar caso o título não seja encontrado
@@ -118,45 +141,54 @@ const Titulo = () => {
           <textarea 
             value={descricaoValue} 
             id='descricao'
-            onChange={handleDescricaoValue}
+            onChange={handleDescricaoChange}
           />
         </div>
 
         <div className='campo-pequeno'>
           <label htmlFor='grupo'>Grupo</label>
-          <input type='text' value={grupo} id='grupo' />
+          <input 
+            type='text' 
+            value={grupoValue} 
+            id='grupo'
+            onChange={handleGrupoChange}
+          />
         </div>
 
         <div className='campo-pequeno'>
           <label htmlFor='subgrupo'>Subgrupo</label>
-          <input type='text' value={subgrupo} id='subgrupo' />
+          <input 
+            type='text' 
+            value={subgrupoValue} 
+            id='subgrupo'
+            onChange={handleSubgrupoChange}
+          />
         </div>
 
-        <div className='campo-pequeno'>
-          <label htmlFor='tipo'>Tipo</label>
-          <input type='text' value={tipo} id='tipo' />
-        </div>
 
         <div className='campo-pequeno'>
           <label htmlFor='dBaixa'>Data de baixa</label>
           <input
             type='text'
-            value={dBaixa ? new Date(dBaixa).toLocaleDateString("pt-BR") : ""}
+            value={dBaixa}
             id='dBaixa'
-          />
+            onChange={handleDBaixaChange}
+            />
         </div>
 
         <div className='campo-pequeno'>
           <label htmlFor='pago'>Valor pago</label>
           <input
             type='text'
-            value={pago ? pago.toLocaleString("pt-BR", { minimumFractionDigits: 2 }) : ""}
+            value={vPago}
             id='pago'
-          />
+            onChange={handleVPagoChange}
+            />
         </div>
 
       </div>
       <div className='rodape'>
+        <p>{tipoValue}</p>
         <p>Parcelas: {parcela} / {tParcelas}</p>
         <div className='campo-agrupamento'>ID Agrupadora {titulo.agrupamento}</div>
       </div>
